a26bd4ab25837bf57ba81cf2e3097461c6603c2f
jonathan-cj <69293681+jonathan-cj@users.noreply.github.com>
2022-04-30 11:45:45 +0700

    add navbar components

---
+++ components/Navigation/Bar.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav class="w-full h-16 bg-primary-1000 flex flex-row justify-between items-center fixed top-0 z-50">
+    <p class="text-secondary-light text-h4 font-bold ml-5">Jonathan</p>
+
+    <section>
+      <ul class="flex justify-evenly text-primary-200">
+        <li class="mr-8">
+          <a href="#">Biography</a>
+        </li>
+        <li class="mr-8">
+          <a href="#">Projects</a>
+        </li>
+        <li class="mr-8">
+          <a href="#">Contacts</a>
+        </li>
+      </ul>
+    </section>
+  </nav>
+</template>
