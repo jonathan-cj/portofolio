7a9f14e4576d28c8abc063372852f1a66846ae51
jonathan-cj <69293681+jonathan-cj@users.noreply.github.com>
2022-04-30 11:46:04 +0700

    add landing icon component

---
+++ components/LandingIcon.vue
@@ -0,0 +1,24 @@
+<template>
+  <div :class="className">
+    <img :src="`${icon}`"/>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    icon: String,
+    bgColor: String
+  },  
+  setup(props) {
+    
+  },
+  computed:{
+    className(){
+      return `rounded-full w-44 h-44 bg-${this.bgColor} flex justify-center items-center`   
+    }
+  }
+})
+</script>
+
