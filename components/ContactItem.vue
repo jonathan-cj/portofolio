f3fe4d70779e98abf23eaf93a84ba8e9dbe38f06
jonathan-cj <69293681+jonathan-cj@users.noreply.github.com>
2022-05-01 10:57:49 +0700

    finish contact section

---
+++ components/ContactItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <NuxtLink 
+    :to='link' target="_blank" 
+    class="flex flex-row mt-5 items-center w-fit"
+  >
+    <img class="lg:max-h-10 max-h-10 h-auto w-auto object-fit" :src="icon" />
+    <p 
+      class="text-primary-0 lg:text-h4 text-p1 font-normal ml-9" 
+    >{{contact}}</p>
+  </NuxtLink>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    contact: { type: String },
+    icon: { type: String },
+    link: { type: String },
+  },
+})
+</script>
--- components/Module/Contact.vue
+++ components/Module/Contact.vue
@@ -1,5 +1,50 @@
 <template>
-  <section id="contact" class="h-[50vh]">
-    <h1>Test</h1>
+  <section id="contact" class="h-[50vh] bg-primary-600 pl-16">
+    <header class="pt-16">
+      <h1 class="text-secondary-light font-semibold text-h1 drop-shadow-neon mt-4">Contact me</h1>
+    </header>
+
+    <section>
+      <ul class="">
+        <li v-for="contact in contacts" :key="contact.id" class="w-fit">
+          <ContactItem :contact="contact.contact" :icon="contact.icon" :link="contact.link" />
+        </li>
+      </ul>
+    </section>
   </section>
 </template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+var id = 0
+
+export default defineComponent({
+  setup() {
+    
+  },
+  data() {
+    return {
+      contacts: [
+        {
+          id: id++,
+          contact: "jonathan-cj",
+          link: "https://www.linkedin.com/in/jonathan-cj-09816521b/",
+          icon: "../assets/images/linkedin.png"
+        },
+        {
+          id: id++,
+          contact: "jonathan-cj",
+          link: "https://github.com/jonathan-cj",
+          icon: "../assets/images/github.png"
+        },
+        {
+          id: id++,
+          contact: "jonathanjahja2401@gmail.com",
+          icon: "../assets/images/email.png"
+        },
+      ]
+    }
+  }
+})
+</script>
+
